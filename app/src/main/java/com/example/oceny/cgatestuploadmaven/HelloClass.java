d8c9aa24a6423f7e585c4dc86e1498333d60cb55
gacao <gacao@iflytek.com>
2019-04-26 10:28:38 +0800

    test 2 library upload

--- app/src/main/java/com/example/oceny/cgatestuploadmaven/HelloClass.java
+++ app/src/main/java/com/example/oceny/cgatestuploadmaven/HelloClass.java
@@ -1,7 +1,11 @@
 package com.example.oceny.cgatestuploadmaven;
 
+import com.example.oceny.testlibrary.LogUtils;
+
 public class HelloClass {
     public void print(){
         System.out.print("hahahahha");
+        new LogUtils().LogDebug("fffff");
     }
+
 }
--- testlibrary/src/main/java/com/example/oceny/testlibrary/LogUtils.java
+++
@@ -1,9 +0,0 @@
-package com.example.oceny.testlibrary;
-
-import android.util.Log;
-
-public class LogUtils {
-    public void LogDebug(String string) {
-        Log.d("tag",string);
-    }
-}
