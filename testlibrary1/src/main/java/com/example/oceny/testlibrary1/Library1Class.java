0d35d84aa98668619e8196f62265adeea876140b
gacao <gacao@iflytek.com>
2019-04-26 10:33:55 +0800

    add test file

---
+++ testlibrary1/src/main/java/com/example/oceny/testlibrary1/Library1Class.java
@@ -0,0 +1,7 @@
+package com.example.oceny.testlibrary1;
+
+public class Library1Class {
+    public static void testClass1(){
+
+    }
+}
---
+++ testlibrary2/src/main/java/com/example/oceny/testlibrary2/Library2Class.java
@@ -0,0 +1,7 @@
+package com.example.oceny.testlibrary2;
+
+public class Library2Class {
+    public static void testClass2(){
+
+    }
+}
